6fc0bc433191d5ebf1e12f1d6be13a6ac2f67d3e
Murtala <39172444+Toyinka@users.noreply.github.com>
2022-07-14 22:21:11 +0000

    attempted challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    
+  const sum = arrayOfNumbers.reduce((x, y) => x + y)
+    return arrayOfNumbers.reduce((x, y) => x + y);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  
+  evenNumbers = arrayOfNumbers.filter((num) => (num %2) === 0)
+    return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
